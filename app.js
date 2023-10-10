3d47f5d9891f5a7b01dbe8cba3e2f7b4b327a92e
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-10-10 14:08:23 +0200

    Tirsdag cookie clicker v. 1.2

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+//1. En funktion så ens click er mere værdifulde, fx at hver gang man clicker giver den 2 i stedet for 1 osv.
+//2. En funktion så den autoclicker og giver x antal pr sekund eller minut.
+//3. En achievement funktion som holder øje med forskellige ting såsom:
+// Hvilke opgraderinger man har købt. Hvor mange cookies man højest har haft
+
+
+
+
+
 let cookies = 0;
-
-function bakeCookie(){
-    cookies++
-    console.log(cookies);
-}
-
+let cps = 0;
+let cps2 = 0;
+let upgradePurchased = false;
+let upgradePurchased2 = false;
+let upgradePurchased3 = false;
+let gordonActive = false;
